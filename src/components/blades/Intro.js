8adf31046c965f6871d67ee94a56e35feeac2a79
Jose Barajas <barajas.jose45@yahoo.com>
2021-09-08 11:50:31 -0700

    component styles

--- src/components/blades/Intro.js
+++ src/components/blades/Intro.js
@@ -1,7 +1,7 @@
 function Intro(props){
     const { title , text} = props;
     return (
-        <section className="intro">
+        <section className="intro container">
             <h2>{title}</h2>
             <p>{text}</p>
         </section>
--- src/components/blades/image-with-text.js
+++ src/components/blades/image-with-text.js
@@ -1,9 +1,13 @@
 function ImageWithText(props){
     const { img , alt, text} = props;
     return (
-        <section className="image-with-text">
-            <img src={img} alt={alt}/>
-            <p>{text}</p>
+        <section className="image-with-text container">
+            <div className='img-container col-lg-6 col-sm-12'>
+                <img src={img} alt={alt}/>                
+            </div>
+            <div className="text-container col-lg-6 col-sm-12">
+                <p>{text}</p>
+            </div> 
         </section>
     )
 
